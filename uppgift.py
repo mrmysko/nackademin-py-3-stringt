ac4bb83d08add1e2a5a9e697f6abf8e098f4fa95
Robin Kavenius <robin.kavenius@yh.nackademin.se>
2024-02-29 13:51:36 +0100

    Finish stringt

--- uppgift.py
+++ uppgift.py
@@ -1,6 +1,16 @@
 # Skriv endast din funktionsdefinition här på denna indenteringsnivå! Det är
 # viktigt att du ger funktionen exakt det namn som står i beskrivningen.
 
+
+def stringt(*args, end="\n", sep=" "):
+    """Return a string."""
+
+    print(out_string)
+
+    out_string = sep.join(args) + end
+    return out_string
+
+
 if __name__ == "__main__":
     # Här kan du skriva testkod för din funktion. Denna körs endast när du kör
     # filen direkt, och inte när du importerar den som modul i en annan fil.
@@ -12,4 +22,7 @@ if __name__ == "__main__":
     #
     # print(funktionsnamn("hejsan", 99))
     # print(funktionsnamn([19, 22, 31, 29, 1])
-    pass  # Ta bort denna rad
+    print(stringt("Det", "här", "är", "en", "sträng", end=" ", sep="-"))
+    print(stringt("Det", "här", "är", "en", "sträng"))
+    print(stringt("Det", "här", "är", "en", "sträng", end="\t", sep="!"))
+    # print(stringt([1, 2, 3, 4], [5, 6, 7, 8], sep="F"))
